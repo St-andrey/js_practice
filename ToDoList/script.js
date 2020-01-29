160fc91fc8b1a2db98eac023d46dac5e18acc1cb
St-andrey <andrey.stepanshin@gmail.com>
2020-01-29 15:30:06 +0300

    first commit

---
+++ ToDoList/script.js
@@ -0,0 +1,10 @@
+const newTask = document.getElementById('inputTask');
+const taskList = [];
+
+newTask.addEventListener('keydown', event =>{
+    console.log("fired");
+//     if (event.key === 'Enter' || event.keyCode === 13){
+// taskList.push(newTask.value);
+//     }
+});
+console.log(taskList);
