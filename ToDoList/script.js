bbca1ac440f9089a93110d336b6750482a6ef218
St-andrey <andrey.stepanshin@gmail.com>
2020-01-29 19:39:17 +0300

    appending elements to list realized

--- ToDoList/script.js
+++ ToDoList/script.js
@@ -1,10 +1,26 @@
 const newTask = document.getElementById('inputTask');
 const taskList = [];
 
-newTask.addEventListener('keydown', event =>{
-    console.log("fired");
-//     if (event.key === 'Enter' || event.keyCode === 13){
-// taskList.push(newTask.value);
-//     }
+newTask.addEventListener('keydown', event => {
+    if (event.key === 'Enter' || event.keyCode === '13') {
+        taskList.push(newTask.value);
+        newTask.value = '';
+        updateList();
+    }
 });
-console.log(taskList);
+
+const ulElement = document.getElementById('taskList');
+
+function updateList() {
+    ulElement.innerHTML = '';
+    for (const newTask of taskList) {
+        const liElement = document.createElement('li');
+        liElement.className = 'taskItem';
+        ulElement.append(liElement);
+        
+        const pElement = document.createElement('p');
+        pElement.className = 'taskText';
+        liElement.append(pElement);
+        liElement.innerText = newTask;
+    }
+}
